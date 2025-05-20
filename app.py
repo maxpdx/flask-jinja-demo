9a5fd3825904038b40f88645afe8627c456747d2
Max <15226023+maxpdx@users.noreply.github.com>
2025-05-19 23:07:09 -0400

    add live reload

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
 
@@ -7,5 +10,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    server = Server(app.wsgi_app)
+    server.watch('templates/*.html')  # Watch all HTML templates
+    server.serve(debug=True, port=5500)  # Or 5000 if you prefer
