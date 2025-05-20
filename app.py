1f817b25bc97c439aa7f2a91313c8c18a8cbb97c
Max <15226023+maxpdx@users.noreply.github.com>
2025-05-19 23:29:36 -0400

    fix TEMPLATES_AUTO_RELOAD

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from livereload import Server
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True  # Ensure templates are auto-reloaded
 
 @app.route("/")
 def home():
